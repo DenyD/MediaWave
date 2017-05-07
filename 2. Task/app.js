bdec8dd1a37f3654f0a7da4eb2f12b08f8b376ff
Daniel Siwczak <dsiwczak@o2.pl>
2017-05-07 13:55:05 +0200

    add files

---
+++ 2. Task/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+                  
+  $('#path').click(function(){
+      if(document.getElementById('list').style.visibility === 'visible') {
+          document.getElementById('list').style.visibility = 'hidden';
+      } else {
+          document.getElementById('list').style.visibility = 'visible';
+      }            
+  });
+});
+      
+      
+      
+      
+            
+            
